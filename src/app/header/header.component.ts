3207b9d83877e1e9534e86ecb4eb369d895bbc70
cami-1310 <al347948@edu.uaa.mx>
2025-04-27 17:53:05 -0600

    instalacion local de bootstrap y mi parte del home

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterModule, NavBarComponent],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
