2a3fe651532a1cf60fe2b2aec97c849ef6d1f900
rafa MQZ <2rafael.marquez.amarillas@gmail.com>
2025-05-01 15:23:16 -0600

    primer terminado de contacto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent,ServiciosComponent,ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: 'servicios', component: ServiciosComponent },
     
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
