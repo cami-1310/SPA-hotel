1405a2d0b50ce7bd37e943bee1cfa3208d55318a
cami-1310 <al347948@edu.uaa.mx>
2025-05-11 09:21:59 -0600

    terminada vista api y bara busqueda

---
+++ src/app/destinos/destinos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { PadreBusquedaComponent } from '../padre-busqueda/padre-busqueda.component';
+
+@Component({
+  selector: 'app-destinos',
+  imports: [PadreBusquedaComponent],
+  templateUrl: './destinos.component.html',
+  styleUrl: './destinos.component.css'
+})
+export class DestinosComponent {
+
+}
